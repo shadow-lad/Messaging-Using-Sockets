29ed043fafb15a554b8b49fac2f98b3597657067
shadow-kage <shardav.lad21@gmail.com>
2020-03-21 03:01:18 +0530

    Added code to add user to database

--- Java Backend/src/main/java/org/shardav/server/handler/RegistrationHandler.java
+++ Java Backend/src/main/java/org/shardav/server/handler/RegistrationHandler.java
@@ -8,6 +8,7 @@ import org.shardav.server.comms.Response;
 import org.shardav.server.comms.Response.ResponseStatus;
 import org.shardav.server.comms.login.UserDetails;
 import org.shardav.server.mail.GMailService;
+import org.shardav.server.sql.Database;
 import org.shardav.utils.Log;
 
 import javax.mail.MessagingException;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.security.GeneralSecurityException;
+import java.sql.SQLException;
 import java.util.Random;
 
 public class RegistrationHandler implements Runnable {
@@ -90,9 +92,20 @@ public class RegistrationHandler implements Runnable {
                     && verify.getString("otp")!=null){
 
                     if(this.OTP.equals(verify.getString("otp"))) {
+
+                        try {
+                            //TODO: Ponder upon how to properly handle this and then do it
+                            Database database = Database.getInstance("root", "toor");
+                            database.insertUser(details);
+                        } catch (SQLException ex){
+                            ex.printStackTrace();
+                            return;
+                        }
+
                         ClientHandler clientHandler = new ClientHandler(client, details.getEmail(), in, out);
                         Server.activeClients.add(clientHandler);
                         new Thread(clientHandler).start();
+
                     } else {
                         Response failed = new Response(ResponseStatus.FAILED, "Invalid OTP");
                         out.println(failed.toJSON());
